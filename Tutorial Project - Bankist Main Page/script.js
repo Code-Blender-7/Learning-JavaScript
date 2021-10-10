d0ff1530ebef4e151eb73bb6b241c6c2c19d4043
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2021-10-10 17:06:58 +0600

    Removed research readlines from the JS files, Modified CSS files, Added and completed Notes#4 of the log, Added smooth animations. [May change the behavior of the animations later on]

--- Tutorial Project - Bankist Main Page/script.js
+++ Tutorial Project - Bankist Main Page/script.js
@@ -6,6 +6,8 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header')
 const title = document.querySelector('title')
+
+document.documentElement.style.setProperty('--color-primary', 'violet')
 ///////////////////////////////////////
 // Modal window
 
@@ -51,86 +53,39 @@ ALL SCRIPT AND WEBSITE RESOURCES ARE COPYRIGHTED BY Jonas Schmedtmann
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////// LECTURES
 
-console.log(document.documentElement); // // Print the entire document.
-console.log(document.head); // // Print the document head section only.
-console.log(document.body); // // Print the document body section only.
 
-
-console.log(document.querySelector('.header')); // return the first element that matches the element
-const allSections = document.querySelectorAll('.section');
-console.log(allSections)
-
-
-document.getElementById('section--1')
-const allButtons  = document.getElementsByTagName('button');
-console.log(allButtons)
-
-// Creating Elements 
+//// //// Cookie message Popup
 
 const message = document.createElement('div'); // Creating a div in the HTML [PROGRAMMED IN JS]
 message.classList.add('cookie-message') // Adding a class to the div  
 
-
-message.innerHTML = 'We use cookies for improved functionality <button class="btn btn--close-cookie">Got it!</button>'
-message.style.background = 'yellow'
+message.innerHTML = `We use cookies for improved functionality <br> And We serve no desert <button class="btn btn--close-cookie">Got it!</button>`
 
 
-
-header.append(message)
-
-header.after(message) // after the header
 header.before(message) // before the header
 
 
-// REMOVING Elements + Changing Stuff
 document.querySelector('.btn--close-cookie').addEventListener('click', function() {
   message.remove()
   title.textContent = "𝙱𝚊𝚗𝚔𝚒𝚜𝚝 | 𝚆𝚑𝚎𝚗 𝙱𝚊𝚗𝚔𝚒𝚗𝚐 𝚖𝚎𝚎𝚝𝚜 𝙼𝚒𝚗𝚒𝚖𝚊𝚕𝚒𝚜"
 })
 
 
-
 // Styles
 
 message.style.backgroundColor = "#37383d"
 message.style.width = "120%"
-
-console.log(message.style.backgroundColor)
-console.log(message.style.width)
-
-
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-
-
 message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 20 + "px";
 
 
-// CSS custom properties
-document.documentElement.style.setProperty('--color-primary', 'orangered')
 
 
-// Attributes
+//// Smooth Button Scrolling Animation
 
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt)
-console.log(logo.src)
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
 
-// Remember that the HTMLElements cannot printout custom attributes that are not standerd.
+btnScrollTo.addEventListener('click', function(e) {
+  section1.scrollIntoView({behavior: 'smooth'})
+})
 
-console.log(logo.designer)
-console.log(logo.className)
-
-// However, you can read the dom by using getAttribute
-console.log(logo.getAttribute('designer'))
-
-// Ways to set attribute
-logo.setAttribute('company', 'bankist')
-
-// Change the attribute value
-logo.alt = "Beautiful minimalist logo"
-
-
-
-// Data Attributes
-console.log(logo.dataset.versionNumber)
