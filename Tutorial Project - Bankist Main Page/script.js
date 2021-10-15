38114f0e1da2480d8e5ca7c84209a553270e25ae
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2021-10-15 12:46:52 +0600

    Updated NavBar Scrolling. Updated logs

--- Tutorial Project - Bankist Main Page/script.js
+++ Tutorial Project - Bankist Main Page/script.js
@@ -106,14 +106,13 @@ btnScrollTo.addEventListener('click', function(e) {
  //  })
  // })
 
-// Event Delegation
-// 1. Add event listener to common parentNode elements
-// 2. Determine what element originated the event
-
 document.querySelector(".nav__links").addEventListener('click', function(e) {
   e.preventDefault()
-  console.log(e.target)
-  if (e.target.classList.contains('nav_link')) {
-    console.log("Linked")
-  }
-})
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior : "smooth"});
+  }}
+)
+
+
+
