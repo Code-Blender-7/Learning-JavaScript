620d10ba7c4e0bdf1f500a043a1539ce729701fa
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2022-04-19 22:19:02 +0600

    added logs, refactored files

---
+++ Mapty-geoworkout-website/development-logs/log_006[Refactor-Project-Architecture].md
@@ -0,0 +1,27 @@
+For those who are still not undestanding what is the purpose of the refactor and this whole story, please nagivate to the logs. the link to the recent log is [here](https://github.com/Code-Blender-7/Learning-JavaScript/blob/main/Mapty-geoworkout-website/development-logs/log_005%5BProject-Architecture%5D.md).
+
+# START
+
+
+I'll try to make it be sure.
+
+
+This is the refactor or more like arranging the messed up portions of the original javascript file. I really don't want to mess up the log writing of this description, so I'll start with the the constructor names and their purpose.
+
+
+The refactoring is simply the transfer of the codelines and put them into constructor functions of the main class. Let the main class is called App. Here, everthing of the following functions, features and all the behavior of the website is simply from this App. Let's begin.
+
+
+The class App consists of 5 sub classes and a constructor. The classes include - 
+
+1. _getPosition(). {*Used for the gettting location of the place where the user actually clicks on the tilemap of the leaflet API. IF you are thinking that it has something to do with geolocation, it's that the geolocation is used to make the tilemap actually load on the user's position. It has nothing to do with the fancy stuff.*} 
+2. _loadMap(position) {*This is the second part of the getPosition. the getPosition only simply transfers the data of the user to the loadMap. You can say that the real magic occurs here.*}
+3. _showForm() {*Used for the purpose of the showing the form. Simply that means to toggle the form if the user had clicked on a position.*}
+4. _toggleElevationField() {*Used for changing the input fields between the running and cycling.*}
+5. _newWorkout() {*After the user fills the creditials of the marker, the new workout is formed. it means that a list is created regarding of the user's details.*}
+
+
+Some of the function uses the data of other classes internally. that's why, the this keyword is used. the global elements are moved to the class and they are assigned as private classes.
+
+
+In the end, call the app by creating a new variable and assigning the value to the App class.
--- Mapty-geoworkout-website/script.js
+++ Mapty-geoworkout-website/script.js
@@ -16,62 +16,83 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
-// access the geolocation....
+class App {
+	//private instances
+	#map
+	#mapEvent
 
-navigator.geolocation.getCurrentPosition(function(position) {
-	console.log(position)
-	const {latitude} = position.coords;
-	const {longitude} = position.coords;
-	const coordiates = [latitude, longitude]
+	constructor() {
+		this._getPosition();
 
-	// the L is called the namespace
-	map = L.map('map').setView(coordiates, 13);
+	form.addEventListener('submit', this._newWorkout.bind(this))
+
+	// change the form input settings (Running/Cycling)
+	inputType.addEventListener('change', this._toggleElevationField)
+
+	}
+		
+	_getPosition() {
+
+		// access the geolocation....
+		navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function() {
+			alert("Could not get your positon. Be sure to activate the location access.")
+		})
+
+	}
+
+	_loadMap(position) {
+		console.log(this)
+		const {latitude} = position.coords;
+		const {longitude} = position.coords;
+		const coordiates = [latitude, longitude]
+
+		// the L is called the namespace
+		this.#map = L.map('map').setView(coordiates, 13);
+
+		// map display settings
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(this.#map);
 
 
-	// map display settings
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+		// responds on click
+		this.#map.on('click', this._showForm.bind(this));
 
+	}
 
-	// responds on click
-	map.on('click', function(mapE) {
-		mapEvent = mapE
-		console.log(mapEvent);
-		const {lat, lng} = mapEvent.latlng;
-		form.classList.remove('hidden') // toggle form
+	_showForm(mapE) {
+		this.#mapEvent = mapE
+		form.classList.remove('hidden') // toggle form after submit
 		inputDistance.focus();
+	}
+
+	_toggleElevationField() {
+		inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+		inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+	}
+
+	_newWorkout(e) {
+
+		e.preventDefault()
+		// clear list
+		inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = ''
+		
+		// add a marker [click]
+		L.marker(this.#mapEvent.latlng)
+			.addTo(this.#map)
+		    .bindPopup(L.popup({ // Marker customization..
+		    	maxWidth: 250,
+		    	minWidth: 100,
+		    	autoClose: false,
+		    	closeOnClick: false,
+		    	className: `cycling-popup`,
+		    }))
+		    .setPopupContent('Workout')
+		    .openPopup();	
+}};
 
 
+// execuetion
+const app = new App()
+app._getPosition()
 
-
-	})
-
-	// if !location access
-}, function() {
-	alert("Could not get your positon. Be sure to activate the location access.")
-})
-
-form.addEventListener('submit', function(e) {
-	e.preventDefault()
-	// clear list
-	inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = ''
-	// add a marker [click]
-	L.marker(mapEvent.latlng)
-		.addTo(map)
-	    .bindPopup(L.popup({ // Marker customization..
-	    	maxWidth: 250,
-	    	minWidth: 100,
-	    	autoClose: false,
-	    	closeOnClick: false,
-	    	className: `cycling-popup`,
-	    }))
-	    .setPopupContent('Workout')
-	    .openPopup();	
-})
-
-
-inputType.addEventListener('change', function() {
-	inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-	inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-})
